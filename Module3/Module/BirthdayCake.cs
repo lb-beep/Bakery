2d145109bcd98c16836d7a91ccd11ec459438458
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-06-02 17:06:55 -0600

    Add files via upload

---
+++ Module3/Module/BirthdayCake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace module3.Module
+{
+    public class BirthdayCake<T>
+    {
+
+        public BirthdayCake()
+        {
+
+
+        }
+        public T Flavor { get; set; }
+
+
+
+    }
+}
+
